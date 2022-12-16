ae9a5bfa08a1fdbde15de6f655b48dcff695080f
adityaKumar <adityakumarrajput01@gmail.com>
2022-12-16 23:48:46 +0530

    seventh commit - react-router setup,fetching news category-wise

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter,
+  Route, Routes
+} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
+      <>
+      <BrowserRouter>
       <div>
-        <Navbar/>
-        <News pagesize={6} country="in" category="science"/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<News key="general" pagesize={6} country="in" category="general"/>}/>
+          <Route path="/business" element={<News key="business" pagesize={6} country="in" category="business"/>}/>
+          <Route path="/entertainment" element={<News key="entertainment" pagesize={6} country="in" category="entertainment"/>}/>
+          <Route path="/health" element={<News key="health" pagesize={6} country="in" category="health"/>}/>
+          <Route path="/science" element={<News key="science" pagesize={6} country="in" category="science"/>}/>
+          <Route path="/sports" element={<News key="sports" pagesize={6} country="in" category="sports"/>}/>
+          <Route path="/technology" element={<News key="technology" pagesize={6} country="in" category="technology"/>}/>
+        </Routes>
       </div>
+      </BrowserRouter>
+      </>
     )
   }
 }
