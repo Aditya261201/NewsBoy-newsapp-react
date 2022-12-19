66f11e3bc6c2c25ba3c38c64055897e3258f6d3a
adityaKumar <adityakumarrajput01@gmail.com>
2022-12-19 23:14:03 +0530

    tenth commit - adding infinite scrollbar by npm pkg,(removing prev/nxt btns)

--- src/Components/News.js
+++ src/Components/News.js
@@ -18,8 +18,6 @@ export class News extends Component {
         category: PropTypes.string
     }
 
-
-    // here we have made a constructor of super class..and we will use states in it...we can also pass the whole array of articles here..but we have stored it earlier and we will use it here by this keyword.
     constructor(props) {
         super(props);
         console.log("Hello I am a constructor from news component");
@@ -49,16 +47,6 @@ export class News extends Component {
     async componentDidMount() {
         this.updateNews();
     }
-    // handlePrevClick = async () => {
-    //     this.setState({ page: this.state.page - 1 })
-    //     this.updateNews();
-    // }
-    // handleNextClick = async () => {
-    //     this.setState({ page: this.state.page + 1 })
-    //     this.updateNews()
-    // };
-
-
 
     fetchMoreData = async () => {
         this.setState({ page: this.state.page + 1})
@@ -66,7 +54,7 @@ export class News extends Component {
         let data = await fetch(url)
         let parsedData = await data.json()
         this.setState({
-            articles: this.state.articles.concat(parsedData.articles),
+            articles: this.state.articles.concat(parsedData.articles),//concating upcoming results with previous ones.
             totalResults: parsedData.totalResults
         })
     }
@@ -76,8 +64,9 @@ export class News extends Component {
         return (
             <>
                 <h1 className="text-center" style={{ margin: '30px 0px 30px 0px' }}>{`NewsBoy-Top Headlines  (${this.props.category})`}</h1>
-                {/* if loading is true then show spinner.*/}
                 {this.state.loading && <Spinner/>}
+                {/* added infinite scrollbar tag from its package docs.
+                its added just above map function..that is determined by docs/examples in docs */}
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
                     next={this.fetchMoreData}
@@ -88,17 +77,12 @@ export class News extends Component {
                     <div className="row my-3">
                         {this.state.articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
-                                {/* ternary operator for both title and description as if they are null set them as empty string */}
                                 <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} date={element.publishedAt} source={element.source.name} />
                             </div>
                         })}
                     </div>
                     </div>
                 </InfiniteScroll>
-                {/* <div className="container d-flex justify-content-between">
-                    <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pagesize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div> */}
             </>
         )
     }
